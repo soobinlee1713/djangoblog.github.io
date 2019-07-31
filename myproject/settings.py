317003673cbbd44a0b0198b2ea6553ecd870e150
unknown <sbean1713@naver.com>
2019-07-31 16:47:48 +0900

    fourth commit

--- myproject/settings.py
+++ myproject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRE_KEY','(t-cn54&n3@xzb-5y!=18wf)b%d)6)d(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get('DJANGO_DEBUG',True))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
